d70177b09621ba18632d1c6bd063bce6a4fc094a
lyubomyr <lyubomyr.fedevitch@eliftech.com>
2018-05-25 17:59:37 +0300

    navigation between main and results activities

--- app/src/main/java/com/lyubomyr/wordscounter/DisplayResultsView.java
+++ app/src/main/java/com/lyubomyr/wordscounter/DisplayResultsView.java
@@ -2,9 +2,13 @@ package com.lyubomyr.wordscounter;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,8 +25,11 @@ public class DisplayResultsView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_results_view);
+        setContentView(R.layout.results_view);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         this.results = intent.getStringExtra("results");
@@ -33,12 +40,45 @@ public class DisplayResultsView extends AppCompatActivity {
         if (null != results){
             Log.d(LOG_TAG, "displaying results");
             Log.d(LOG_TAG, results);
-            Toast.makeText(getApplicationContext(), "got results!!!", Toast.LENGTH_SHORT);
-            Toast.makeText(getApplicationContext(), results, Toast.LENGTH_LONG);
 
             TextView resultsTextView = (TextView) findViewById(R.id.resultsText);
             resultsTextView.setText(results);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Log.d(LOG_TAG, "item selected");
+        Log.d(LOG_TAG, String.valueOf(item.getItemId()));
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                /*
+                if (NavUtils.shouldUpRecreateTask(this.getParent(), this.getIntent())) {
+                    Log.d(LOG_TAG, "recreate");
+                    // This activity is NOT part of this app's task, so create a new task
+                    // when navigating up, with a synthesized back stack.
+                    TaskStackBuilder.create(this.getParent())
+                            // Add all of this activity's parents to the back stack
+                            .addNextIntentWithParentStack(this.getParent().getParentActivityIntent())
+                            // Navigate up to the closest parent
+                            .startActivities();
+                } else {
+                    Log.d(LOG_TAG, "nav");
+                    //This activity is part of this app's task, so simply
+                    //navigate up to the logical parent activity.
+                    NavUtils.navigateUpTo(this, this.getParentActivityIntent());
+                }
+                */
+                //Log.d(LOG_TAG, "home");
+
+                //NavUtils.navigateUpFromSameTask(this);
+
+                super.onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/lyubomyr/wordscounter/MainView.java
+++ app/src/main/java/com/lyubomyr/wordscounter/MainView.java
@@ -1,10 +1,13 @@
 package com.lyubomyr.wordscounter;
 
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -14,12 +17,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 public class MainView extends AppCompatActivity {
 
@@ -31,16 +29,25 @@ public class MainView extends AppCompatActivity {
     private EditText inputText;
     private DrawerLayout mDrawerLayout;
 
+    private Intent showResults;
+    private PendingIntent pendingIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enter_text_view);
+        setContentView(R.layout.main_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         inputText = (EditText) findViewById(R.id.inputText);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        this.showResults = new Intent(this, DisplayResultsView.class);
+        this.pendingIntent = TaskStackBuilder.create(this)
+                // add all of DetailsActivity's parents to the stack,
+                // followed by DetailsActivity itself
+                .addNextIntentWithParentStack(showResults).getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
         //wordCounter = new WordCounter();
 
@@ -126,63 +133,11 @@ public class MainView extends AppCompatActivity {
     }
 
     protected void showResultsView(String result) {
-        Intent showResults = new Intent(MainView.this, DisplayResultsView.class);
-        showResults.putExtra("results", result);
-        MainView.this.startActivity(showResults);
+
+        this.showResults.putExtra("results", result);
+        startActivity(this.showResults);
+
     }
-
-    // obsolete method
-
-//    protected void displayResults(String results){
-//        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        //mainLayout.setVisibility(View.INVISIBLE);
-//        EditText inputText = (EditText) findViewById(R.id.inputText);
-//        inputText.setVisibility(View.INVISIBLE);
-//
-//        final int resultsLayoutId = View.generateViewId();
-//        final int resultsBackButtonId = View.generateViewId();
-//
-//        LinearLayout resultsLayout = new LinearLayout(this);
-//        resultsLayout.setOrientation(LinearLayout.HORIZONTAL);
-//        resultsLayout.setMinimumHeight(mainLayout.getHeight());
-//        resultsLayout.setMinimumWidth(mainLayout.getWidth());
-//        resultsLayout.setId(resultsLayoutId);
-//
-//        TextView resultsView = new TextView(this);
-//        resultsView.setText(results);
-//        resultsLayout.addView(resultsView);
-//
-//        Button hideResultsButton = new Button(this);
-////        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(100 /*width*/, 50 /*height*/);
-////        layoutParams.setMargins(0, 0, 0, 0 /*int left, int top, int right, int bottom*/);
-////        hideResultsButton.setLayoutParams(layoutParams);
-//        hideResultsButton.setText("Back");
-//
-//        hideResultsButton.setId(resultsBackButtonId);
-//
-//        hideResultsButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                LinearLayout resultsLayout = (LinearLayout) findViewById(resultsLayoutId);
-//                ViewGroup resultsLayoutParent = (ViewGroup) resultsLayout.getParent();
-//                if(null != resultsLayoutParent) //for safety only  as you are doing onClick
-//                    resultsLayoutParent.removeView(resultsLayout);
-//
-//                EditText inputText = (EditText) findViewById(R.id.inputText);
-//                inputText.setVisibility(View.VISIBLE);
-//
-//                Button hideResultsButton = (Button) findViewById(resultsBackButtonId);
-//                ViewGroup backButtonParent = (ViewGroup) hideResultsButton.getParent();
-//                if(null != backButtonParent) //for safety only  as you are doing onClick
-//                    backButtonParent.removeView(hideResultsButton);
-//            }
-//        });
-//
-//        toolbar.addView(hideResultsButton);
-//
-//        mainLayout.addView(resultsLayout);
-//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -193,20 +148,8 @@ public class MainView extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        /*
-        //obsolete
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-        */
+        Log.d(LOG_TAG, "item selected");
+        Log.d(LOG_TAG, String.valueOf(item.getItemId()));
 
         switch (item.getItemId()) {
             case android.R.id.home:
