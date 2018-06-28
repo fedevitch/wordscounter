baea149a658b0707b77ec45f9d8edec08c163217
lyubomyr <lyubomyr.fedevitch@eliftech.com>
2018-06-28 15:43:04 +0300

    added google charts

--- app/src/main/java/com/lyubomyr/wordscounter/DisplayResultsView.java
+++ app/src/main/java/com/lyubomyr/wordscounter/DisplayResultsView.java
@@ -10,7 +10,11 @@ import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.Xml;
 import android.view.MenuItem;
+import android.webkit.MimeTypeMap;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -75,6 +79,23 @@ public class DisplayResultsView extends AppCompatActivity {
         String resultString = getResultString(results);
         TextView resultsTextView = (TextView) findViewById(R.id.resultsText);
         resultsTextView.setText(resultString);
+
+        final WebView chartWebView = findViewById(R.id.chart_webview);
+
+
+        chartWebView.getSettings().setJavaScriptEnabled(true);
+
+        chartWebView.loadUrl("file:android_asset/chart.html");
+
+        chartWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url){
+                Log.d(LOG_TAG, "Chart loading finished");
+                Log.d(LOG_TAG, "Trying to execute js");
+
+                chartWebView.evaluateJavascript(getChartWebViewData(), null);
+            }
+        });
 
         handleSaveResult(results);
 
@@ -181,5 +202,62 @@ public class DisplayResultsView extends AppCompatActivity {
         }
 
     }
+
+    private String getChartWebViewData(){
+        return
+                "console.log('started'); console.log(JSON.stringify(google)); console.log(google.charts);" +
+                //"var loadChart = function(){" +
+                "      google.charts.load('current', {'packages':['treemap']});\n" +
+                "      google.charts.setOnLoadCallback(drawChart);\n" +
+                "      function drawChart() {\n" +
+                        "console.log('drawChart called...');" +
+                "        var data = google.visualization.arrayToDataTable([\n" +
+                "          ['Location', 'Parent', 'Market trade volume (size)', 'Market increase/decrease (color)'],\n" +
+                "          ['Global',    null,                 0,                               0],\n" +
+                "          ['America',   'Global',             0,                               0],\n" +
+                "          ['Europe',    'Global',             0,                               0],\n" +
+                "          ['Asia',      'Global',             0,                               0],\n" +
+                "          ['Australia', 'Global',             0,                               0],\n" +
+                "          ['Africa',    'Global',             0,                               0],\n" +
+                "          ['Brazil',    'America',            11,                              10],\n" +
+                "          ['USA',       'America',            52,                              31],\n" +
+                "          ['Mexico',    'America',            24,                              12],\n" +
+                "          ['Canada',    'America',            16,                              -23],\n" +
+                "          ['France',    'Europe',             42,                              -11],\n" +
+                "          ['Germany',   'Europe',             31,                              -2],\n" +
+                "          ['Sweden',    'Europe',             22,                              -13],\n" +
+                "          ['Italy',     'Europe',             17,                              4],\n" +
+                "          ['UK',        'Europe',             21,                              -5],\n" +
+                "          ['China',     'Asia',               36,                              4],\n" +
+                "          ['Japan',     'Asia',               20,                              -12],\n" +
+                "          ['India',     'Asia',               40,                              63],\n" +
+                "          ['Laos',      'Asia',               4,                               34],\n" +
+                "          ['Mongolia',  'Asia',               1,                               -5],\n" +
+                "          ['Israel',    'Asia',               12,                              24],\n" +
+                "          ['Iran',      'Asia',               18,                              13],\n" +
+                "          ['Pakistan',  'Asia',               11,                              -52],\n" +
+                "          ['Egypt',     'Africa',             21,                              0],\n" +
+                "          ['S. Africa', 'Africa',             30,                              43],\n" +
+                "          ['Sudan',     'Africa',             12,                              2],\n" +
+                "          ['Congo',     'Africa',             10,                              12],\n" +
+                "          ['Zaire',     'Africa',             8,                               10]\n" +
+                "        ]);\n" +
+                "\n" +
+                "        tree = new google.visualization.TreeMap(document.getElementById('chart_div'));\n" +
+                "\n" +   "console.log('drawing tree');" +
+                "        tree.draw(data, {\n" +
+                "          minColor: '#f00',\n" +
+                "          midColor: '#ddd',\n" +
+                "          maxColor: '#0d0',\n" +
+                "          headerHeight: 15,\n" +
+                "          fontColor: 'black',\n" +
+                "          showScale: true\n" +
+                "        });\n" +
+                "\n" + "console.log('done');" +
+                //"      }\n"+
+                "      }\n";
+    }
+
+
 
 }
