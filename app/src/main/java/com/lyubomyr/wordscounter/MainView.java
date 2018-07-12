3a645dab3ece10e38d86d6801480015ecbebabe7
lyubomyr <lyubomyr.fedevitch@eliftech.com>
2018-07-12 12:36:43 +0300

    parse shared html page

--- app/src/main/java/com/lyubomyr/wordscounter/MainView.java
+++ app/src/main/java/com/lyubomyr/wordscounter/MainView.java
@@ -1,14 +1,11 @@
 package com.lyubomyr.wordscounter;
 
-import android.app.PendingIntent;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.NavUtils;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -18,11 +15,16 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.URLUtil;
 import android.widget.EditText;
 
 import com.lyubomyr.wordscounter.Storage.SettingsViewModel;
 
-import java.io.Serializable;
+import org.jsoup.Jsoup;
+import org.w3c.dom.Document;
+
+import java.io.IOException;
+
 
 public class MainView extends AppCompatActivity {
 
@@ -42,8 +44,8 @@ public class MainView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_view);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        inputText = (EditText) findViewById(R.id.inputText);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        inputText = findViewById(R.id.inputText);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
@@ -53,19 +55,17 @@ public class MainView extends AppCompatActivity {
 
         this.showResults = new Intent(this, DisplayResultsView.class);
 
-        //wordCounter = new WordCounter();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startCount(view);
+                startCount();
             }
         });
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
@@ -100,34 +100,34 @@ public class MainView extends AppCompatActivity {
                     }
                 });
 
-        mDrawerLayout.addDrawerListener(
-                new DrawerLayout.DrawerListener() {
-                    @Override
-                    public void onDrawerSlide(View drawerView, float slideOffset) {
-                        // Respond when the drawer's position changes
-                        //Log.d(LOG_TAG, "slide");
-                    }
-
-                    @Override
-                    public void onDrawerOpened(View drawerView) {
-                        // Respond when the drawer is opened
-                        //Log.d(LOG_TAG, "opened");
-                    }
-
-                    @Override
-                    public void onDrawerClosed(View drawerView) {
-                        // Respond when the drawer is closed
-                        //Log.d(LOG_TAG, "closed");
-                    }
-
-                    @Override
-                    public void onDrawerStateChanged(int newState) {
-                        // Respond when the drawer motion state changes
-                        //Log.d(LOG_TAG, "changed");
-                        //Log.d(LOG_TAG, String.valueOf(newState));
-                    }
-                }
-        );
+//        mDrawerLayout.addDrawerListener(
+//                new DrawerLayout.DrawerListener() {
+//                    @Override
+//                    public void onDrawerSlide(View drawerView, float slideOffset) {
+//                        // Respond when the drawer's position changes
+//                        //Log.d(LOG_TAG, "slide");
+//                    }
+//
+//                    @Override
+//                    public void onDrawerOpened(View drawerView) {
+//                        // Respond when the drawer is opened
+//                        //Log.d(LOG_TAG, "opened");
+//                    }
+//
+//                    @Override
+//                    public void onDrawerClosed(View drawerView) {
+//                        // Respond when the drawer is closed
+//                        //Log.d(LOG_TAG, "closed");
+//                    }
+//
+//                    @Override
+//                    public void onDrawerStateChanged(int newState) {
+//                        // Respond when the drawer motion state changes
+//                        //Log.d(LOG_TAG, "changed");
+//                        //Log.d(LOG_TAG, String.valueOf(newState));
+//                    }
+//                }
+//        );
 
         initializeSettings();
 
@@ -164,7 +164,7 @@ public class MainView extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    protected void startCount(View view) {
+    private void startCount() {
         wordCounter = new WordCounter();
         text = inputText.getText().toString();
 
@@ -172,19 +172,6 @@ public class MainView extends AppCompatActivity {
 
         showResultsView(wordCounter.getCountResult());
 
-//        Snackbar.make(view, "Calculating words, please wait...", Snackbar.LENGTH_INDEFINITE)
-//                .setAction("", null).show();
-//
-//        Snackbar.make(view, "Words calculated", Snackbar.LENGTH_INDEFINITE)
-//                .setAction("Show results",new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        //displayResults(wordCounter.getResult());
-//                        String result = wordCounter.getResult();
-//                        Log.d(LOG_TAG, result);
-//                        showResultsView(result);
-//                    }
-//                }).show();
     }
 
     protected void showResultsView(CountResult countResult) {
@@ -206,8 +193,6 @@ public class MainView extends AppCompatActivity {
 
                 Log.d(LOG_TAG, "Settings set");
 
-                //
-
             }
         };
 
@@ -228,14 +213,49 @@ public class MainView extends AppCompatActivity {
     private void handleTextSendTo(Intent intent){
         String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
         if(sharedText != null) {
-            Log.d(LOG_TAG, sharedText);
-            inputText.setText(sharedText);
+            if(URLUtil.isValidUrl(sharedText)){
+                handleHtmlSendTo(sharedText);
+            } else {
+                Log.d(LOG_TAG, sharedText);
+                inputText.setText(sharedText);
+            }
         }
     }
 
-    private void handleHtmlSendTo(Intent intent){
-        String sharedText = intent.getStringExtra(Intent.EXTRA_HTML_TEXT);
-        Log.d(LOG_TAG, sharedText);
+    private void handleHtmlSendTo(String link){
+
+        Log.d(LOG_TAG, "Parsing web page");
+        Log.d(LOG_TAG, link);
+
+        JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+        jsoupAsyncTask.execute(link);
+
+    }
+
+    private class JsoupAsyncTask extends AsyncTask<String, Void, Void> {
+
+        private String htmlContentInStringFormat;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                org.jsoup.nodes.Document htmlDocument = Jsoup.connect(params[0]).get();
+                htmlContentInStringFormat = htmlDocument.text();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            inputText.setText(htmlContentInStringFormat);
+        }
     }
 
     private void openSettings(){
